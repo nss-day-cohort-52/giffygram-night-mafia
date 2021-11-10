3d886a668b836509a9cf0f9918bc544197ee79a5
Jubjub422 <ross.stovenour@gmail.com>
2021-11-10 13:37:57 -0600

    first commit, getting a couple of posts to show up.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,20 @@
 import { GiffyGram } from "./GiffyGram.js"
 import { LoginForm } from "./auth/Login.js"
-import { fetchUsers } from "./data/provider.js"
+import { fetchUsers, fetchPosts } from "./data/provider.js"
 
 const applicationElement = document.querySelector(".giffygram")
 // export was below
     const renderApp = () => {
     const user = parseInt(localStorage.getItem("gg_user"))
     fetchUsers()
-
-
+    .then(()=>fetchPosts())
+    .then(()=> {
     if (user) {
         applicationElement.innerHTML = GiffyGram()
     } else {
         applicationElement.innerHTML = LoginForm()
     }
+})
 }
 renderApp()
 
